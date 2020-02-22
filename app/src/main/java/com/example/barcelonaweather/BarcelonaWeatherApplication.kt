62b1cd6b99c433e2648cc753513bb97c20ecc71f
Marcos Fuentes Aguilera <marcos.fuentes.aguilera@everis.com>
2020-02-22 17:11:17 +0100

    Added koin helper and koin dependencies

--- app/src/main/java/com/example/barcelonaweather/BarcelonaWeatherApplication.kt
+++ app/src/main/java/com/example/barcelonaweather/BarcelonaWeatherApplication.kt
@@ -1,13 +1,12 @@
 package com.example.barcelonaweather
 
 import android.app.Application
-import com.example.mvvmcurrencyconverter.di.KoinHelper.startKoin
+import com.example.barcelonaweather.di.KoinHelper.startKoin
 
 class BarcelonaWeatherApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin(this)
-
     }
 
 
