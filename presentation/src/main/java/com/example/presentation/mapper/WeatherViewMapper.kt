e485df7b264b77e7ad54ff6f587b513d11f0e98c
Marcos Fuentes Aguilera <marcos.fuentes.aguilera@everis.com>
2020-02-23 12:46:25 +0100

    Created mapper to map the response to the local model

---
+++ presentation/src/main/java/com/example/presentation/mapper/WeatherViewMapper.kt
@@ -0,0 +1,60 @@
+package com.example.presentation.mapper
+
+import com.example.presentation.model.WeatherLocal
+import com.example.service.model.*
+
+class WeatherViewMapper {
+    fun mapTo(weatherResponse: WeatherResponse): WeatherLocal {
+        return WeatherLocal(
+            coordMapper(weatherResponse.coord),
+            weatherElementListMapper(weatherResponse.weather),
+            weatherResponse.base,
+            mainMapper(weatherResponse.main),
+            weatherResponse.visibility,
+            windMapper(weatherResponse.wind),
+            cloudMapper(weatherResponse.clouds),
+            weatherResponse.dt,
+            sysMapper(weatherResponse.sys),
+            weatherResponse.timezone,
+            weatherResponse.id,
+            weatherResponse.name,
+            weatherResponse.cod
+        )
+    }
+
+    private fun coordMapper(coord: Coord) = CoordLocal(coord.lon, coord.lat)
+
+    private fun cloudMapper(clouds: Clouds) = CloudsLocal(clouds.all)
+
+    private fun mainMapper(main: Main) = MainLocal(
+        main.temp,
+        main.feelsLike,
+        main.tempMin,
+        main.tempMax,
+        main.pressure,
+        main.humidity
+    )
+
+    private fun sysMapper(sys: Sys) =
+        SysLocal(sys.type, sys.id, sys.message, sys.country, sys.sunrise, sys.sunset)
+
+    private fun weatherElementListMapper(weatherElementList: List<WeatherElement>): List<WeatherElementLocal> {
+        val weatherList: ArrayList<WeatherElementLocal> = ArrayList()
+
+        for (weatherElement in weatherElementList) {
+            weatherList.add(weatherElementMapper(weatherElement))
+        }
+
+        return weatherList
+    }
+
+    private fun weatherElementMapper(weatherElement: WeatherElement) = WeatherElementLocal(
+        weatherElement.id,
+        weatherElement.main,
+        weatherElement.description,
+        weatherElement.icon
+    )
+
+    private fun windMapper(wind: Wind) = WindLocal(wind.speed, wind.deg)
+
+}
